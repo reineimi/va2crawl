5ce3314fd938705084ee36221cb969c03d106918
霊音・永旻 <reineimi.dev@gmail.com>
2024-10-31 21:00:34 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 rm va2bash.lua;
 rm va2crawl.lua;
 rm crawl.lua;
-curl -LO https://reineimi.github.io/va2/lib/va2bash.lua
-curl -LO https://reineimi.github.io/va2/lib/va2crawl.lua
-curl -LO https://reineimi.github.io/va2crawl/crawl.lua
-echo 'reineimi > va2crawl updated!'
+curl -LO https://reineimi.github.io/va2/lib/va2bash.lua;
+curl -LO https://reineimi.github.io/va2/lib/va2crawl.lua;
+curl -LO https://raw.githubusercontent.com/reineimi/va2crawl/refs/heads/main/crawl.lua;
+echo 'reineimi > va2crawl updated!';
